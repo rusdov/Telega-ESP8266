9fa38059ff8f2d59072c7edd4b20147e827e2acd
Dovgan Ruslan <43816598+rusdov@users.noreply.github.com>
2019-12-13 18:50:17 +0300

    Create auth.h

---
+++ auth.h
@@ -0,0 +1,5 @@
+// Initialize Wifi connection to the router
+char ssid[] = "XXXXXXXXX";     
+char password[] = "XXXXXXXX"; 
+//telegram token
+#define BOTtoken "933262641:XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  
