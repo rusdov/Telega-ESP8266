e154ebc446fa0acb2c6516f7b794ed9dca6bffb1
Dovgan Ruslan <43816598+rusdov@users.noreply.github.com>
2020-03-31 01:44:36 +0300

    Create timers.h

---
+++ timers.h
@@ -0,0 +1,7 @@
+ timer.setInterval(1000, msensor);
+ timer.setInterval(100, pmpStop);
+ timer.setInterval(10000, pmpStart);
+ timer.setInterval(1000, lightStop);
+ timer.setInterval(1000, chkNewMessages);
+ timer.setInterval(1000, tankAlarm);
+ timer.setInterval(1000,GetDistance);
